1d7b1fa6b9c9bf361180679a3663802ccfa6ada9
Maricar Walters <walterskai7@gmail.com>
2021-01-30 22:30:27 -0600

    added palindrome challenge

---
+++ palindrome/palindromeChallenge/palindromeChallenge/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace palindromeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // to char array
+            // remove extra characters & spaces, probably use regex
+            // then slice one string at a time 
+            // and move first letter to last
+            // then compare original and final variable
+
+        }
+    }
+}
