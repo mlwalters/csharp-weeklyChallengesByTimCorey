6a7f24600241f2c0e77e2a9f3592c0a7147a02fc
Maricar Walters <walterskai7@gmail.com>
2021-01-30 22:36:05 -0600

    updated palindrome

--- palindrome/palindromeChallenge/palindromeChallenge/Program.cs
+++ palindrome/palindromeChallenge/palindromeChallenge/Program.cs
@@ -7,7 +7,9 @@ namespace palindromeChallenge
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            // sample strings:
+            // racecar
+            //also add integers
             // to char array
             // remove extra characters & spaces, probably use regex
             // then slice one string at a time 
